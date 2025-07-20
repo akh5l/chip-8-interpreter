a68a1da809ba5550e1e1abdf0adcbc157abac710
akh5l <xdteddypro@gmail.com>
2025-07-20 23:08:13 +0100

    Improved legibility of chip8_execute() and added a few more operations

--- src/chip8.c
+++ src/chip8.c
@@ -1,6 +1,7 @@
 #include "../include/chip8.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define ROM_START 0x200
 #define SUPER_CHIP false
@@ -85,6 +86,13 @@ uint16_t chip8_fetch(chip8* c8) {
 
 void chip8_execute(chip8* c8, uint16_t opcode) {
 
+  uint8_t x = (opcode & 0x0F00) >> 8;
+  uint8_t y = (opcode & 0x00F0) >> 4;
+  
+  uint8_t n = opcode & 0x000F;
+  uint8_t nn = opcode & 0x00FF;
+  uint16_t nnn = opcode & 0x0FFF;
+
   // isolates first hex character to categorise opcode
   switch (opcode & 0xF000) {
     case 0x0000:
@@ -97,110 +105,113 @@ void chip8_execute(chip8* c8, uint16_t opcode) {
       break;
     
     case 0x1000:
-      c8->PC = opcode & 0x0FFF; // PC jumps to 0x0NNN
+      c8->PC = nnn; // PC jumps to 0x0NNN
       break;
     
     case 0x2000:
       stack_push(&c8->stack, (void*)(uintptr_t)c8->PC); // store current PC value and jump to 0x0NNN
-      c8->PC = opcode & 0x0FFF;
+      c8->PC = nnn;
       break;
 
     case 0x3000:
-      if (c8->V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) { // the following 4 are conditional skips
+      if (c8->V[x] == nn) { // the following 4 are conditional skips
         c8->PC += 2;
       }
       break;
     
     case 0x4000:
-      if (c8->V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
+      if (c8->V[x] != nn) {
         c8->PC += 2;
       }
       break;
 
     case 0x5000:
-      if (c8->V[(opcode & 0x0F00) >> 8] == c8->V[(opcode & 0x00F0) >> 4]) {
+      if (c8->V[x] == c8->V[y]) {
         c8->PC += 2;
       }
       break;
     
     case 0x9000:
-      if (c8->V[(opcode & 0x0F00) >> 8] != c8->V[(opcode & 0x00F0) >> 4]) {
+      if (c8->V[x] != c8->V[y]) {
         c8->PC += 2;
       }
       break;
 
     case 0x6000:
-      c8->V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF; // set VX to NN
+      c8->V[x] = nn; // set VX to NN
       break;
     
     case 0x7000:
-      c8->V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF; // add NN to VX
+      c8->V[x] += nn; // add NN to VX
       break;
     
     case 0x8000: // logical & arithmetic instructions
       switch (opcode & 0x000F) {
         case 0:
-          c8->V[(opcode & 0x0F00) >> 8] = c8->V[(opcode & 0x00F0) >> 4]; // VX set equal to VY
+          c8->V[x] = c8->V[y]; // VX set equal to VY
           break;
         case 1:
-          c8->V[(opcode & 0x0F00) >> 8] |= c8->V[(opcode & 0x00F0) >> 4]; // VX set to VX bitwise OR VY
+          c8->V[x] |= c8->V[y]; // VX set to VX bitwise OR VY
           break;
         case 2:
-          c8->V[(opcode & 0x0F00) >> 8] &= c8->V[(opcode & 0x00F0) >> 4]; // VX set to VX bitwise AND VY
+          c8->V[x] &= c8->V[y]; // VX set to VX bitwise AND VY
           break;
         case 3:
-          c8->V[(opcode & 0x0F00) >> 8] ^= c8->V[(opcode & 0x00F0) >> 4]; // VX set to VX bitwise XOR VY
+          c8->V[x] ^= c8->V[y]; // VX set to VX bitwise XOR VY
           break;
         case 4: // add
-          c8->V[0xF] = (c8->V[(opcode & 0x0F00) >> 8] + c8->V[(opcode & 0x00F0) >> 4]) > 255; // trigger flag if overflow
-          c8->V[(opcode & 0x0F00) >> 8] += c8->V[(opcode & 0x00F0) >> 4];
+          c8->V[0xF] = (c8->V[x] + c8->V[y]) > 255; // trigger flag if overflow
+          c8->V[x] += c8->V[y];
           break;
         case 5: // subtract
-          c8->V[0xF] = c8->V[(opcode & 0x0F00) >> 8] >= c8->V[(opcode & 0x00F0) >> 4] ? 1 : 0; // no borrow = 1
-          c8->V[(opcode & 0x0F00) >> 8] = c8->V[(opcode & 0x0F00) >> 8] - c8->V[(opcode & 0x00F0) >> 4]; // wraps naturally unsigned
+          c8->V[0xF] = c8->V[x] >= c8->V[y] ? 1 : 0; // no borrow = 1
+          c8->V[x] -= c8->V[y]; // wraps naturally unsigned
           break;
         case 7:
-          c8->V[0xF] = c8->V[(opcode & 0x00F0) >> 4] >= c8->V[(opcode & 0x0F00) >> 8] ? 1 : 0; // no borrow = 1
-          c8->V[(opcode & 0x0F00) >> 8] = c8->V[(opcode & 0x00F0) >> 4] - c8->V[(opcode & 0x0F00) >> 8]; // wraps naturally unsigned
+          c8->V[0xF] = c8->V[y] >= c8->V[x] ? 1 : 0; // no borrow = 1
+          c8->V[x] = c8->V[y] - c8->V[x]; // wraps naturally unsigned
           break;
         case 6: // shift depending on super-chip config or not
           if (SUPER_CHIP) {
-            c8->V[(opcode & 0x0F00) >> 8] = c8->V[(opcode & 0x00F0) >> 4];
+            c8->V[x] = c8->V[y];
           }
-          c8->V[0xF] = c8->V[(opcode & 0x0F00) >> 8] & 0x1;
-          c8->V[(opcode & 0x0F00) >> 8] >>= 1;
+          c8->V[0xF] = c8->V[x] & 0x1;
+          c8->V[x] >>= 1;
           break;
         case 0xE:
           if (SUPER_CHIP) {
-            c8->V[(opcode & 0x0F00) >> 8] = c8->V[(opcode & 0x00F0) >> 4];
+            c8->V[x] = c8->V[y];
           }
           c8->V[0xF] = c8->V[(opcode & 0x0F00) & 0x80] >> 7;
-          c8->V[(opcode & 0x0F00) >> 8] <<= 1;
+          c8->V[x] <<= 1;
           break;
       }
       break;
 
     case 0xA000:
-      c8->I = opcode & 0x0FFF; // I (index) set to NNN
+      c8->I = nnn; // I (index) set to NNN
       break;
 
     case 0xB000:
-      c8->PC = opcode & 0x0FFF;
-      c8->PC += SUPER_CHIP ? c8->V[(opcode & 0x0F00) >> 8] : c8->V[0];
+      c8->PC = nnn;
+      c8->PC += SUPER_CHIP ? c8->V[x] : c8->V[0];
+      break;
+
+    case 0xC000:
+      c8->V[x] = (rand() % 256) & nn;
       break;
     
     case 0xD000: // writing to display
-      uint8_t x = c8->V[(opcode & 0x0F00) >> 8]; // get x from VX and wrap around screen with modulo
-      uint8_t y = c8->V[(opcode & 0x00F0) >> 4]; // same as above but VY and y
-      uint8_t n = opcode & 0x000F;
+      uint8_t dx = c8->V[x]; // get x from VX and wrap around screen with modulo
+      uint8_t dy = c8->V[y]; // same as above but VY and y
 
       c8->V[0xF] = 0;
 
       for (int row = 0; row < n; row++) {
         uint8_t sprite_byte = c8->memory[c8->I + row];
-        uint8_t pixel_y = (y + row) % HEIGHT;
+        uint8_t pixel_y = (dy + row) % HEIGHT;
         for (int col = 0; col < 8; col++) { // col < 8 as it's just going thru the byte
-          uint8_t pixel_x = (x + col) % WIDTH;
+          uint8_t pixel_x = (dx + col) % WIDTH;
           uint8_t sprite_pixel = (sprite_byte >> (7 - col)) & 1;
 
           uint16_t idx = (pixel_y * WIDTH + pixel_x);
@@ -213,7 +224,21 @@ void chip8_execute(chip8* c8, uint16_t opcode) {
           }
         }
       }
+      break;
 
+    case 0xE000: // skip one instruction if key corresponding to VX is pressed
+      if (nn == 0x009E) {
+
+      } else { // above but skip if not pressed
+
+      }
+      break;
+    case 0xF000:
+      
+      break;
+
+    default:
+      fprintf(stderr, "Opcode not recognised: %#04x\n", opcode);
       break;
   }
 }
